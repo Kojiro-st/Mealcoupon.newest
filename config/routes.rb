8c8b696425d5511ae60579857e1021b178b9d5ad
Kojiro-st <tenmadetobe5a@gmail.com>
2020-03-14 11:34:41 +0900

    10th(searchproduction.left)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # get 'messages/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'posts#index'
-  resources :posts, except: :index
+  root to: 'messages#index'
+  resources :posts
   resources :users, only: :show
+  resources :messages, only: :index
 end
