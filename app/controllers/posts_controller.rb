4fe9c423c012c42281231f620bd1d20720278788
Kojiro-st <tenmadetobe5a@gmail.com>
2020-04-07 14:29:48 +0900

    検索ページで3項目ずつ配置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(3)
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(9)
   end
 
   def new
