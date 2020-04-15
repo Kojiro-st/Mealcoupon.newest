07002f0d46641e0dac025d1302a5bde5d366faa9
Kojiro-st <tenmadetobe5a@gmail.com>
2020-04-15 14:41:18 +0900

    unicorn変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,18 @@
 # ../が一つ増えている
-app_path = File.expand_path('../../../', __FILE__)
+rails_root = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 # currentを指定
-working_directory "#{app_path}/current/config/unicorn.rb"
+# working_directory "#{app_path}/current"
+working_directory rails_root
 
 # それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
+
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
