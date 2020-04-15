1318ace95c71c22f04904676de9dd0bc32f0e9eb
Kojiro-st <tenmadetobe5a@gmail.com>
2020-04-15 14:21:38 +0900

    unicorn編集

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 # currentを指定
-working_directory "#{app_path}/current"
+working_directory "#{app_path}/current/config/unicorn.conf"
 
 # それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
