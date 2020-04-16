16cedc4f38699cbaebd4c3e39dc79ac4366322a1
Kojiro-st <tenmadetobe5a@gmail.com>
2020-04-16 14:09:12 +0900

    完了

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,28 +1,4 @@
 $(function(){
-  $('.content').css("opacity","0");
-  $(window).scroll(function (){
-    $(".content").each(function(){
-      var imgPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > imgPos - windowHeight + windowHeight/5){
-        $("content",this).css("opacity","1" );
-        $("content",this).css({
-          "width": "90%",
-          "margin": "0 auto",
-          "background-color": "white",
-          "border-radius": "5px"
-        });
-      } else {
-        $("content",this).css("opacity","0" );
-        $("content",this).css({
-          "width": "90%",
-          "margin": "0 auto",
-          "background-color": "white",
-          "border-radius": "5px"
-        });
-      }
-    });
+  $('#contents').hide().fadeIn(1000, function() {
   });
-});
-  
+})
