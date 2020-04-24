e2d553117bf94f42eb2a8292f69828715cab482b
Kojiro-st <tenmadetobe5a@gmail.com>
2020-04-24 22:26:29 +0900

    Revert "自動デプロイ用修正完了"
    This reverts commit cf9a9bcb772f3a75277d7426981f4f6bfc57774e.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,57 +1,3 @@
-# # config valid only for current version of Capistrano
-# # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-# lock '3.13.0'
-
-# # Capistranoのログの表示に利用する
-# set :application, 'Mealcoupon.newest'
-
-# # どのリポジトリからアプリをpullするかを指定する
-# set :repo_url,  'git@github.com:Kojiro-st/Mealcoupon.newest.git'
-
-# # バージョンが変わっても共通で参照するディレクトリを指定
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
-# set :rbenv_type, :user
-# set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
-
-# # どの公開鍵を利用してデプロイするか
-# set :ssh_options, auth_methods: ['publickey'],
-#                   keys: '~/.ssh/sk42lax.pem' 
-
-# # プロセス番号を記載したファイルの場所
-# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-
-# # Unicornの設定ファイルの場所
-# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-# set :keep_releases, 5
-
-# # secrets.yml用のシンボリックリンクを追加
-# set :linked_files, %w{ config/secrets.yml }
-
-# # master.keyの読み込み
-# set :linked_files, fetch(:linked_files, []).push("config/master.key")
-
-# # デプロイ処理が終わった後、Unicornを再起動するための記述
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     invoke 'unicorn:restart'
-#   end
-
-#   desc 'upload secrets.yml'
-#   task :upload do
-#     on roles(:app) do |host|
-#       if test "[ ! -d #{shared_path}/config ]"
-#         execute "mkdir -p #{shared_path}/config"
-#       end
-#       upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-#     end
-#   end
-#   before :starting, 'deploy:upload'
-#   after :finishing, 'deploy:cleanup'
-# end
-
-
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.13.0'
@@ -70,17 +16,20 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                         keys: '~/.ssh/sk42lax.pem'
+                  keys: '~/.ssh/sk42lax.pem' 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# master.keyの読み込み
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
-
 # Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
+
+# master.keyの読み込み
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
@@ -88,4 +37,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
