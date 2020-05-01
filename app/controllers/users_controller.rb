a46a308b50bdf7468acf666ec708791bac62884e
Kojiro-st <tenmadetobe5a@gmail.com>
2020-05-01 13:23:47 +0900

    コントローラー組み替え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
     @posts = @user.posts
-    # @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
 end
