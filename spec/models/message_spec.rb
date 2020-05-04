97fb3de4b853450e5bcd9041c344513c505b73de
Kojiro-st <tenmadetobe5a@gmail.com>
2020-05-04 23:47:59 +0900

    テスト改善

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Message do
+  describe '#create' do
+    it "nameがないと登録できないこと" do
+      message = build(:message, name: nil)
+      message.valid?
+      expect(message.errors[:name]).to include("can't be blank")
+    end
+
+    it "phonenumberがないと登録できないこと" do
+      message = build(:message, phonenumber: nil)
+      message.valid?
+      expect(message.errors[:phonenumber]).to include("can't be blank")
+    end
+
+    it "ordertimeleftがないと登録できないこと" do
+      message = build(:message, ordertimeleft: nil)
+      message.valid?
+      expect(message.errors[:ordertimeleft]).to include("can't be blank")
+    end
+
+    it "ordertimerightがないと登録できないこと" do
+      message = build(:message, ordertimeright: nil)
+      message.valid?
+      expect(message.errors[:ordertimeright]).to include("can't be blank")
+    end
+
+    it "howtoeatがないと登録できないこと" do
+      message = build(:message, howtoeat: nil)
+      message.valid?
+      expect(message.errors[:howtoeat]).to include("can't be blank")
+    end
+
+    it "peopleがないと登録できないこと" do
+      message = build(:message, people: nil)
+      message.valid?
+      expect(message.errors[:people]).to include("can't be blank")
+    end
+
+  end
 end
